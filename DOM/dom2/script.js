32d54c7c5fa52b5f8952f7247efb5a4b273f91c0
AKcs2310 <ankitkrgupta21@gmail.com>
2025-07-12 23:14:39 +0530

    added event listener

--- DOM/dom2/script.js
+++ DOM/dom2/script.js
@@ -2,7 +2,12 @@
   div.style.backgroundColor="green";
   div.style.fontSize="20px";
 //   div.style.visibility="hidden";
-
+let newheading=document.createElement("h1");
+ newheading.innerText="hi my name is ankit";
+ document.querySelector("body").prepend(newheading);
+let para=document.querySelector("p");
+ para.remove();
+ 
 
 
  
